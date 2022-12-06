8e144efe5a01def4b60d9b0586eca077b8b48b0c
MINH_VN <tqminh@b-create.jp>
2022-12-06 17:21:54 +0700

    fix

---
+++ components/Layout.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + "- Amazona" : "Amazona"}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center justify-between shadow-md">
+            <Link href="/">
+              <span className="text-lg font-bold">Amazona</span>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <span className="p-2">Cart</span>
+              </Link>
+              <Link href="/login">
+                <span className="p-2">Login</span>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container  m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright &copy; 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+}
